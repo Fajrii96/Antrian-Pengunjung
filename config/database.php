53e2a017afe99737939c3755583e93a57642cb66
Syarifuddin Fajri <80385024+Fajrii96@users.noreply.github.com>
2025-10-01 01:54:37 +0700

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+$sql_details = array(
+    'user' => 'root',
+    'pass' => '',           // kosongkan password
+    'db'   => 'antrian',  // pastikan database ini sudah kamu buat di phpMyAdmin
+    'host' => 'localhost'
+);
+$con = $sql_details;
+?>
